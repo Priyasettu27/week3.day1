2bd061600e247c836186fc72a8f352700ec8e64c
Priyasettu27 <93431628+Priyasettu27@users.noreply.github.com>
2021-11-13 22:40:45 +0530

    Add files via upload

---
+++ IndexOfString.java
@@ -0,0 +1,15 @@
+package week3.day1;
+
+public class IndexOfString {
+
+	public static void main(String[] args) {  
+		String str = "Java Exercise";
+		int str1 = str.length();
+		System.out.println("The length of the string is:"+" "+str1);
+		int index=str.indexOf('E');
+		int index1=str.indexOf('s');
+	      System.out.printf("'E' is at index %d\n" , index);
+		  System.out.printf("'s' is at index %d\n", index1);
+	}}
+		
+       
